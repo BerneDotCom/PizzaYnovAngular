d2a7ac19bc2d1ac12ac006f10440fbfbca840af2
Bernedotcom <val13120@hotmail.fr>
2017-11-09 12:33:34 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './pizza/pizza.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pizza/pizza.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza',
+  templateUrl: './pizza.component.html',
+  styleUrls: ['./pizza.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PizzaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
