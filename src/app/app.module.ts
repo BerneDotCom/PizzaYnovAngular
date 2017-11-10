d86f1b39e8444184ac77d3dfb0b3955586a1ae05
Bernedotcom <val13120@hotmail.fr>
2017-11-10 12:47:59 +0100

    add router link active

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { APP_ROUTES_PROVIDER } from './app.routes';
 
+//App components
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { IngredientComponent } from './ingredient/ingredient.component';
 import { BucketComponent } from './bucket/bucket.component';
 import { PizzaItemComponent } from './pizza-item/pizza-item.component';
 import { PizzaAdminComponent } from './pizza-admin/pizza-admin.component';
+import { FormIngredientComponent } from './form-ingredient/form-ingredient.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import { PizzaAdminComponent } from './pizza-admin/pizza-admin.component';
     BucketComponent,
     PizzaItemComponent,
     PizzaAdminComponent,
+    FormIngredientComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-ingredient/form-ingredient.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IngredientService } from '../ingredient/services/ingredient.service';
+import { Ingredient } from '../ingredient/models/ingredient';
+import { ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-form-ingredient',
+  templateUrl: './form-ingredient.component.html',
+  styleUrls: ['./form-ingredient.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [IngredientService]
+})
+
+export class FormIngredientComponent implements OnInit {
+  form: FormGroup;
+  ingredient: Ingredient;
+
+  constructor(private ingredientService: IngredientService, public route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    /**
+    * Initialise form
+    */
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.minLength(3)),
+      weight: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit(){
+    this.ingredient = this.form.value;
+    this.ingredientService.create(this.ingredient);
+    this.router.navigateByUrl('/ingredients');
+  }
+}
